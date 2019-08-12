48d5f3fba2db02162e5a8bffe16593b2b3cdb2f0
xperthut <mhmethun@gmail.com>
2019-08-12 09:52:17 -0700

    Fixing naming issue

---
+++ Website/application/controllers/Toview.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Toview extends CI_Controller {
+
+	
+	public function index()
+	{
+            getShared();
+            $_share = new Shared();
+            $data = array(
+                "rand"=>rand(-1000000, 10000000),
+                "fileList"=>$_share->getAllCsvFiles(),
+                "toFileList"=>$_share->getAllJsonFiles()
+                );
+		$this->load->view('topoview', $data);
+	}
+}
